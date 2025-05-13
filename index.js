6c7d00cf00bdbccd2ee9656404667eac02b9831e
Aayushi Sharma <aayushisharma20@navgurukul.org>
2025-05-14 00:43:14 +0530

    Added Put Delete API

--- index.js
+++ index.js
@@ -8,12 +8,11 @@ app.use(express.json());
 let tasks = [];
 let currentId = 1;
 
-// GET all tasks
 app.get('/tasks', (req, res) => {
   res.status(200).json(tasks);
 });
 
-// POST new task
+
 app.post('/tasks', (req, res) => {
   const { title, description } = req.body;
 
@@ -26,8 +25,10 @@ app.post('/tasks', (req, res) => {
   res.status(201).json(newTask);
 });
 
+
+
 app.get('/tasks/:id', (req, res) => {
-  const taskId = parseInt(req.params.id); // id ko number me convert karo
+  const taskId = parseInt(req.params.id); 
   const task = tasks.find(t => t.id === taskId);
 
   if (!task) {
@@ -38,7 +39,34 @@ app.get('/tasks/:id', (req, res) => {
 });
 
 
-// Error handling
+
+app.put("/tasks/:id", (req, res) => {
+  const { title, description } = req.body;
+  const task = tasks.find(t => t.id === parseInt(req.params.id));
+  if (!task) return res.status(404).json({ message: "Task not found" });
+
+  if (!title || !description) {
+    return res.status(400).json({ message: "Title and Description are required" });
+  }
+
+  task.title = title;
+  task.description = description;
+
+  res.status(200).json(task);
+});
+
+
+
+app.delete("/tasks/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = tasks.findIndex(t => t.id === id);
+  if (index === -1) return res.status(404).json({ message: "Task not found" });
+
+  tasks.splice(index, 1);
+  res.status(204).send(); 
+});
+
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Internal server error', error: err.message });
